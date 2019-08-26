ae44eaa3eed40403aa529a7272f16f66b274e5c2
yt0379 <yt0379@qq.com>
2019-08-26 22:47:38 +0800

    组件化实战

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <CompCommunicate /> --> 
+    <TreeTest></TreeTest>
+    <!-- <KFormTest></KFormTest> -->
+    <!-- <HelloWorld 
+      @foo="onFoo"
+      ref="hw"
+      msg="Welcome to Your Vue.js App"/>
+    <HelloWorld>
+      <template>abc</template>
+      <template v-slot:content>lalala...</template>
+      <template v-slot:footer="slotProps">
+        {{slotProps.foo}} - {{slotProps.bar}}
+      </template>
+    </HelloWorld>-->
+    <!-- <CompCommunicate /> -->
     <!-- <SlotsTest></SlotsTest> -->
-    <Recursion></Recursion>
+    <!-- <Recursion></Recursion> -->
   </div>
 </template>
 
@@ -12,14 +24,32 @@ import HelloWorld from "./components/HelloWorld.vue";
 import CompCommunicate from "./components/communicate";
 import SlotsTest from "./components/slots";
 import Recursion from "./components/recursion";
+import KFormTest from "./components/form";
+import TreeTest from './components/tree';
 
 export default {
   name: "app",
+  provide() {
+    return {
+      foo: this
+    };
+  },
   components: {
     HelloWorld,
     CompCommunicate,
     SlotsTest,
-    Recursion
+    Recursion,
+    KFormTest,
+    TreeTest
+  },
+  // mounted() {
+  //   console.log(this.$refs.hw.msg);
+  //   console.log(this.$children[0].msg);
+  // },
+  methods: {
+    onFoo(msg) {
+      console.log(msg);
+    }
   }
 };
 </script>
